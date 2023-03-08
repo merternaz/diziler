fd778ca6647607c66f0604ccaa20e7599d69a12d
Mert Ernaz <rnzappdroid@gmail.com>
2023-03-08 22:56:47 +0300

    first dizi

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace forLoobBr
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            string[] hayvanlar={"aslan","kedi","zebra"};
+            Console.WriteLine(hayvanlar[1]);
+
+            int[] sayilar=new int[10];
+            int toplamSayilar=0;
+
+            for(int i=0;i<sayilar.Length;i++){
+                sayilar[i]=i+1;
+                toplamSayilar+=sayilar[i];
+
+            }
+
+            Console.WriteLine("ortalama:"+toplamSayilar/sayilar.Length);
+
+
+
+        }
+    }
+}
